d4178b71fdceca0252ff72ecd652ef56b1b91a7e
ekaearth111 <crystal@ekafrontier.io>
2026-02-09 22:04:33 +0100

    fix: let workflow handle basePath automatically

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/west-papua-care",
-  assetPrefix: "/west-papua-care",
   images: {
     unoptimized: true,
   },
