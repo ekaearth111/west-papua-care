012a7e11075558c51bd50406a1a207998b725506
ekaearth111 <crystal@ekafrontier.io>
2026-02-09 22:00:55 +0100

    fix: add basePath and assetPrefix for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/west-papua-care",
+  assetPrefix: "/west-papua-care",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
